7a364d7decc1ad10236cd2d4e27ca30568551f9e
najmusskb <najmussakib173@gmail.com>
2022-09-19 02:31:25 -0700

    distructing

--- countries.js
+++ countries.js
@@ -1,10 +1,13 @@
 const countries =()=>{
 
+
     fetch('https://restcountries.com/v3.1/all')
     .then(res=>res.json())
     .then(data=>displayCountries(data))
-}
 
+
+
+}
 const displayCountries=countries=> {
     console.log(countries);
     // daata zodi array hoy tahole amader map korte hobe 
@@ -12,17 +15,22 @@ const displayCountries=countries=> {
     // console.log(AllcountrieHTML[0]);
     const container=document.getElementById('countries');
     container.innerHTML=AllcountrieHTML.join(' ');
-
 }
+
 const GetCountriesHTML=country=>{
+    console.log(country);
+
+// Object distructure
+    const {name,flags}=country
 
     return `
     <div class="style">
-    <h2>${country.name.common}</h2>
-    <img src="${country.flags.png}">
+    <h2>${name.common}</h2>
+    <img src="${flags.png}">
     </div>
     
     `
 }
+
 
 countries()
